114e723400ac1ce4ef9de19d9ff92a882776d620
Roman Efimov <romaonthego@gmail.com>
2013-05-20 16:21:10 -0500

    Add example project

---
+++ REPagedScrollViewExample/REPagedScrollViewExample/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  REPagedScrollViewExample
+//
+//  Created by Roman Efimov on 5/20/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ REPagedScrollViewExample/REPagedScrollViewExample/RootViewController.m
@@ -0,0 +1,38 @@
+//
+//  RootViewController.m
+//  REPagedScrollViewExample
+//
+//  Created by Roman Efimov on 5/20/13.
+//  Copyright (c) 2013 Roman Efimov. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
